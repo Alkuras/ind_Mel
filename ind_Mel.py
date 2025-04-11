32faa1415a37a9ef225c6dfa47a08c569c7e0a18
opilane <opilane@HP-CZC2349HTB>
2025-04-11 11:42:44 +0300

    1

--- ind_Mel.py
+++ ind_Mel.py
@@ -1,1 +1,63 @@
+from statistics import *
+from random import *
+def patsiendid(nimed:list,D:list,c:int)->list:
+    """
+    """
+try:
+    c=3
+    nimed=[]
+    D=[]
+    for l in range(c):
+        while True:
+            n=input("Имя:")
+            if n.isalpha():
+
+                nimed.append(n)
+                break
+            else:
+                print("Пиши имя словами")
+            
+        a=randint(1,30)
+        D.append(a)
+    while True:
+        v=int(input("1. Составить список пациентов с недостатком витамина D и вывести его на экран\n2. Средний показатель витамина \n3. Отобразить список выбранного кол-ва человек с  самым большим показателем\n4. Осуществить поиск пациентов по имени и вывести на экран его результат\n5. Выход\n"))
+        if v==1:
+            print(D)
+            tulemus=False
+            for i in range(c):
+               if D[i]<30:
+                   
+                   
+                   print(nimed[i])
+                   print(D[i])
+                   tulemus=True
+            if tulemus==False:
+                print("Количество D витамна в крови пациентов в норме!")
+
+            
+        elif v==2:
+            D_avg= mean(D)
+            print(f"Средний показатель витамина D {D_avg}")
+        elif v==3:
+            print(3)
+        elif v==4:
+            name=input("Имя пациента:  ")
+            leitud=False
+            for j in range(c):
+              if nimed[j]==name:
+                print(f"{name} содержание витамина D: {D[j]}")
+                leitud=True
+            if leitud==False:
+                print("Данных нет")
+        elif v==5:
+            break
+
+       
+        print(D)
+        print(nimed)
+except Exception as e:
+         print ("Viga:",e)
+
+
+
 
