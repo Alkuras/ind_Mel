de68205b02fc0b09e88194bebcdbfa85ad084c4d
opilane <opilane@HP-CZC2349HTB>
2025-04-14 10:07:10 +0300

    4

--- ind.exe.py
+++ ind.exe.py
@@ -1,4 +1,40 @@
 from ind_Mel import *
 patsiendid=[]
 tulemused=[]
-patsiendid_D(patsiendid,tulemused)
+
+patsiendid_D_siseta_andmed(patsiendid,tulemused)
+
+
+
+
+
+while True:
+    v=int(input("1. Составить список пациентов с недостатком витамина D и вывести его на экран\n2. Средний показатель витамина \n3. Отобразить список выбранного кол-ва человек с  самым большим показателем\n4. Осуществить поиск пациентов по имени и вывести на экран его результат\n5. Удалить данные пациента из списка\n6. Выход\n"))
+    if v==1:
+        
+            patsiendid_D_below_30(patsiendid,tulemused)
+            
+        
+            
+                
+
+            
+    elif v==2:
+        
+                patsiendid_D_avg(patsiendid,tulemused)
+                
+
+    elif v==3:
+                patsiendid_D_top(patsiendid,tulemused)
+    elif v==4:
+                patsiendid_D_find(patsiendid,tulemused)
+    elif v==5:
+                patsiendid_D_del(patsiendid,tulemused)
+
+
+    elif v==6:
+        break
+print(patsiendid)
+print(tulemused)
+
+
--- ind_Mel.py
+++ ind_Mel.py
@@ -1,94 +1,153 @@
 
+
 from statistics import *
 from random import *
 
-def patsiendid_D(nimed:list,D:list)->list:
+def patsiendid_D_siseta_andmed(nimed:list,D:list,)->list:
     """Составляет или дополняет  список пациентов и их результатов анализов на витамин D
+    :nimed:list: Список имён
+    :D:list: Список результатов анализов
+    :c:int: Количество пациентов вводимое в список
+    
+    """
+    try:
+        
+        while True:
+            c=int(input("Количество пациентов, вводимое в список: "))
+        
+            if c>0:
+                break
+            else:
+                print("Количество должно быть положительным")
+        
+        for l in range(c):
+            while True:
+                n=input("Имя:")
+                if n.isalpha():
+
+                    nimed.append(n)
+                    break
+                else:
+                    print("Пиши имя словами")
+            
+            a=randint(1,100)
+            D.append(a)
+        return D,nimed,c
+            
+    except Exception as e:
+            print ("Viga:",e)
+
+def patsiendid_D_below_30(nimed:list,D:list)->list:
+    """Составляет список пациентов с недостатком витамина D
     :nimed:list: Список имён
     :D:list: Список результатов анализов
     
     """
-try:
-    while True:
-        c=int(input("Количество пациентов, вводимое в список: "))
+
+    
+           
+            
+            
+    tulemus=False
+    for i in range(len(nimed)):
+                   if D[i]<30:
+                   
+                   
+                       print(nimed[i])
+                       print(D[i])
+                       tulemus=True
+                   
+    if tulemus==False:
+                    print("Количество D витамна в крови пациентов в норме!")
+                    
+    
+def patsiendid_D_avg(nimed:list,D:list)->list:
+    """Выдаёт средний показатель витамина D
+    :nimed:list: Список имён
+    :D:list: Список результатов анализов
+    """
+
+    D_avg= mean(D)
+    print(f"Средний показатель витамина D {D_avg}")       
+            
         
-        if c>0:
-            break
-        else:
-            print("Количество должно быть положительным")
-    nimed=[]
-    D=[]
-    for l in range(c):
-        while True:
-            n=input("Имя:")
-            if n.isalpha():
 
-                nimed.append(n)
-                break
-            else:
-                print("Пиши имя словами")
-            
-        a=randint(1,100)
-        D.append(a)
-    while True:
-        v=int(input("1. Составить список пациентов с недостатком витамина D и вывести его на экран\n2. Средний показатель витамина \n3. Отобразить список выбранного кол-ва человек с  самым большим показателем\n4. Осуществить поиск пациентов по имени и вывести на экран его результат\n5. Выход\n"))
-        if v==1:
-            print(D)
-            tulemus=False
-            for i in range(c):
-               if D[i]<30:
-                   
-                   
-                   print(nimed[i])
-                   print(D[i])
-                   tulemus=True
-            if tulemus==False:
-                print("Количество D витамна в крови пациентов в норме!")
+def patsiendid_D_top(nimed:list,D:list)->list:
+    """Составляет топ Т пациентов с наилучшими показателями витамина D
+    :nimed:list: Список имён
+    :D:list: Список результатов анализов
+    
+    """
 
             
-        elif v==2:
-            D_avg= mean(D)
-            print(f"Средний показатель витамина D {D_avg}")
-        elif v==3:
-            T=int(input("Сколько пациентов с наилучшими показателями хотите видеть?: "))
-            if T>0:
+    T=int(input("Сколько пациентов с наилучшими показателями хотите видеть?: "))
+    if T>0:
              
              
-                 a=0 
-                 pal=D.copy()
-                 pal.sort(reverse=True)
-                 nam=[]
-                 for l in range(T):
+                     
+                     pal=D.copy()
+                     pal.sort(reverse=True)
+                     nam=[]
+                     for l in range(T):
                 
-                    ind=D.index(pal[a])
+                        ind=D.index(pal[l])
 
-                    nam.append(nimed[ind])
-                    a+=1
+                        nam.append(nimed[ind])
+                        
  
-                 print("Лучшие")
-                 for x in range(T):
+                     print("Лучшие")
+                     for x in range(T):
 
-                        print(f"{nam[x]} - {pal[x]}")
-            else:
-                    print("Напиши число")
-        elif v==4:
-            name=input("Имя пациента:  ")
-            leitud=False
-            for j in range(c):
-              if nimed[j]==name:
-                print(f"{name} содержание витамина D: {D[j]}")
-                leitud=True
-            if leitud==False:
-                print("Данных нет")
-        elif v==5:
-            break
+                            print(f"{nam[x]} - {pal[x]}")
+                     
+    else:
+                        print("Напиши число")
+def patsiendid_D_find(nimed:list,D:list)->list:
+    """Осуществляет поиск по имени пациента
+    :nimed:list: Список имён
+    :D:list: Список результатов анализов
+    
+    """
+    name=input("Имя пациента:  ")
+    leitud=False
+    for j in range(len(nimed)):
+                  if nimed[j]==name:
+                    print(f"{name} содержание витамина D: {D[j]}")
+                    leitud=True
+
+                    
+                  if leitud==False:
+                    print("Данных нет")
+def patsiendid_D_del(nimed:list,D:list)->list:
+    """Удалить пациента из списка
+    :nimed:list: Список имён
+    :D:list: Список результатов анализов
+    
+    """
+    name=input("Введи имя пациента, которого необходимо удалить: ")
+    
+    if name.isalpha():
+                    k=nimed.count(name)
+                    if k>0:
+                        for j in range(k):
+
+                            ind=nimed.index(name)
+                            D.pop(ind)
+                            nimed.remove(name)
+                        return D,nimed
+                        
+                        
+                    else:
+                        print("Данные отсутствуют")
+        
+
+
+            
             
 
-       
-        print(D)
-        print(nimed)
-except Exception as e:
-         print ("Viga:",e)
+        
+    # except Exception as e:
+    #         print ("Viga:",e)
 
 
 
